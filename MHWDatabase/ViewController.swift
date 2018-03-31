aed0fb2d05d5609825a13367a9fb67a194be09ea
Neil Shetty <neil@humaninc.com>
2018-03-31 10:59:25 -0700

    Initial Commit

---
+++ MHWDatabase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MHWDatabase
+//
+//  Created by Neil Shetty on 3/31/18.
+//  Copyright © 2018 Neil Shetty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
